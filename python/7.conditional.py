66fd7dc7da074afae011c22a78980a3bef9fb154
s2angji <s2angji@gmail.com>
2022-03-29 14:51:21 +0900

    20220329 파이썬 교육

---
+++ python/7.conditional.py
@@ -0,0 +1,20 @@
+print(1)
+if True:
+    print(2)
+    print(3)
+print(4)
+
+print(1)
+if False:
+    print(2)
+    print(3)
+print(4)
+
+print(1)
+if True:
+    print(2.1)
+    print(2.1)
+else:
+    print(2.2)
+    print(2.2)
+print(4)
